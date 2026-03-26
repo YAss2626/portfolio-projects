ba2cd097174ab5359807fd53d6cc66e9f660bfb7
Yacine DOSSO <ydoss086@uottawa.ca>
2026-03-26 11:33:02 -0400

    Add e-Hotels web application with PHP backend and SQL database

--- e-hotels-web-app/Requete sql/query_hotel_list.sql
+++
@@ -1,4 +0,0 @@
-SELECT "Hotel"."ID", "Hotel".nom_hotel, "Hotel".adress, "Hotel".category, 
-"Hotel".hotel_chain_id
-FROM "Hotel" 
-JOIN "hotel_chain" ON "hotel_chain"."id " = "Hotel".hotel_chain_id;
--- e-hotels-web-app/Requete sql/query_miseAJour_prixChambre.sql
+++
@@ -1,3 +0,0 @@
-UPDATE public.chambre
-SET price = CASE WHEN 150 >= 0 THEN 150 ELSE price END
-WHERE "ID" = 1;
